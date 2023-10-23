ecc7f52aaeca85483b5e965065f477c05a5dee8c
nimishsara12 <nimishsarathe561@gmail.com>
2023-10-23 16:28:47 +0530

    start

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+
+
+const app =express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+    "mongodb+srv://nimishsarathe561:myproject1@recipes.6qynhki.mongodb.net/recipes?retryWrites=true&w=majority"
+    );
+app.listen(3001, ()=>console.log("SERVER STARTES"));
